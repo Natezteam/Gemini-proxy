bd9f224a8f6633f2a706beb297fa3f2ebf7c4625
Natezteam <america112ptu@gmail.com>
2025-06-29 14:32:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
 )
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent"
+GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
 
 @app.post("/gemini")
 async def chat(request: Request):
